41d483d2e307d18424a64d05539e11fd5cd99249
oksanaliu <lyuklyan4@icloud.com>
2025-09-23 23:47:32 +0200

    feat(categories): add model/service/controller and route

--- app.js
+++ app.js
@@ -1,30 +1,32 @@
-import express from "express";
-import morgan from "morgan";
-import cors from "cors";
-import path from "path";
-import "dotenv/config";
-import "./db/sequelize.js";
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import path from 'path';
+import 'dotenv/config';
+import './db/sequelize.js';
+import categoriesRouter from './routes/categoriesRouter.js';
 
-import healsRouter from "./routes/healthRouter.js";
+import healsRouter from './routes/healthRouter.js';
 
 const { APP_PORT = 3000 } = process.env;
 
 const app = express();
 
-app.use(morgan("tiny"));
+app.use(morgan('tiny'));
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static(path.resolve("public")));
+app.use(express.static(path.resolve('public')));
 
-app.use("/api", healsRouter);
+app.use('/api', healsRouter);
+app.use('/api/categories', categoriesRouter);
 
 app.use((_, res) => {
-  res.status(404).json({ message: "Route not found" });
+  res.status(404).json({ message: 'Route not found' });
 });
 
 app.use((err, req, res, next) => {
-  const { status = 500, message = "Server error" } = err;
+  const { status = 500, message = 'Server error' } = err;
   res.status(status).json({ message });
 });
 
---
+++ db/Categorie.js
@@ -0,0 +1,24 @@
+import { DataTypes, Model } from 'sequelize';
+import { sequelize } from './sequelize.js';
+
+export class Category extends Model {}
+
+Category.init(
+  {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    modelName: 'Category',
+    tableName: 'categories',
+    timestamps: false,
+  }
+);
