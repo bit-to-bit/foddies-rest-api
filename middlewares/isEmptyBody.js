b5eda599ebfbe22c7005c0199914cbe1d960eb1c
bit-to-bit <vasiliy.pykhteev@gmail.com>
2025-09-23 22:05:40 +0300

    init-repository

---
+++ middlewares/isEmptyBody.js
@@ -0,0 +1,11 @@
+import httpError from "../helpers/httpError.js";
+
+const isEmptyBody = (req, res, next) => {
+  const { length } = Object.keys(req.body);
+  if (!length) {
+    return next(httpError(400, "Body must have at least one key"));
+  }
+  next();
+};
+
+export default isEmptyBody;
---
+++ routes/healthRouter.js
@@ -0,0 +1,8 @@
+import express from "express";
+import { checkHealth } from "../controllers/healthControllers.js";
+
+const healsRouter = express.Router();
+
+healsRouter.get("/ping", checkHealth);
+
+export default healsRouter;
