c6a091c74d626bb6dacd9f1e71b2cfd7af3e68eb
oksanaliu <lyuklyan4@icloud.com>
2025-09-25 13:36:37 +0200

    fixed models

--- db/Testimonial.js
+++ db/Testimonial.js
@@ -6,12 +6,12 @@ export const Testimonial = sequelize.define(
   'Testimonial',
   {
     id: {
-      type: DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV4,
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
       primaryKey: true,
     },
     ownerId: {
-      type: DataTypes.STRING(24),
+      type: DataTypes.INTEGER,
       allowNull: false,
     },
     testimonial: {
